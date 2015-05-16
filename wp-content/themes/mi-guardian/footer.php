1342a752167b4e10705b35cec8a505ac90be02f1
Dan Shoreman <dan@danshoreman.co.uk>
2015-05-16 22:04:43 +0100

    latest

--- wp-content/themes/mi-guardian/footer.php
+++ wp-content/themes/mi-guardian/footer.php
@@ -1,11 +1,13 @@
 	<footer class="pageFooter">
-		<div class="row">
-			<div class="small-12 medium-6 columns">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/general/mi-guardian-logo.svg" class="footerLogo" alt="">
-			</div>
-			<div class="small-12 medium-6 columns">
-				<p>For further information<br>
-				Call 0800 130 0011</p>
+		<div class="footerInner">
+			<div class="row">
+				<div class="small-12 medium-6 columns">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/general/mi-guardian-logo.svg" class="footerLogo" alt="">
+				</div>
+				<div class="small-12 medium-6 columns">
+					<p class="contacts">For further information<br>
+					Call <span class="heavy">0800 130 0011</span></p>
+				</div>
 			</div>
 		</div>
 	</footer>
--- wp-content/themes/mi-guardian/page-home.php
+++ wp-content/themes/mi-guardian/page-home.php
@@ -45,7 +45,8 @@
 	<section class="signUp">
 		<div class="row">
 			<div class="small-12 columns">
-				<h4>Sign up for all the latest news and offers</h4>
+				<!--
+<h4>Sign up for all the latest news and offers</h4>
 				<form id="signupForm" method="">
 					<fieldset class="">
 						
@@ -62,10 +63,39 @@
 						<input  type="submit" value="Send" class="submitBtn">
 					</fieldset>
 				</form>
+-->
+				
+				<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+				<form action="//wearevast.us5.list-manage.com/subscribe/post?u=16dec5b34f91bdc514cd89088&amp;id=36e7aedb99" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					<h4>Sign up for all the latest news and offers</h4>
+				<div class="indicates-required"><span class="asterisk">*</span> indicates a required field</div>
+				<div class="mc-field-group inputWrap">
+					<label for="mce-NAME">Name  <span class="asterisk">*</span></label>
+					<input type="text" value="" name="NAME" class="required input" id="mce-NAME" placeholder="Name">
+				</div>
+				
+				<div class="mc-field-group inputWrap">
+					<label for="mce-EMAIL">Email  <span class="asterisk">*</span></label>
+					<input type="email" value="" name="EMAIL" class="required email input" id="mce-EMAIL" placeholder="Email">
+				</div>
+				
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_16dec5b34f91bdc514cd89088_36e7aedb99" tabindex="-1" value=""></div>
+				    <input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button submitBtn">
+				  </div>
+				</form>
+				</div>
+				
+				<!--End mc_embed_signup-->
 			</div>
 		</div>
 	</section>
-	<section class="threeThink">
+	<section class="thinkBlock">
 		<div class="row">
 			<div class="small-12 medium-4 columns">
 				<div class="thinkContent">
@@ -108,28 +138,38 @@
 			<p class="large">Free 8 week trial</p>
 		</div>
 	</section>
-	<section class="fourThink">
-		<div class="row">
-			<div class="small-12 columns">
-				<h5>FROM ONLY 50P A DAY.<br>IT REALLY IS A DEAL WORTH THINKING ABOUT</h5>
+	<section class="dealBlock">
+		<div class="dealInner">
+			<div class="row">
+				<div class="small-12 columns">
+					<h5 class="title">FROM ONLY 50P A DAY.<br>IT REALLY IS A DEAL WORTH THINKING ABOUT</h5>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="small-6 medium-3 columns">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/homepage/portable-alarm.png" class="" alt="">
-				<p>Portable Alarm</p>
-			</div>
-			<div class="small-6 medium-3 columns">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/homepage/home-installation.png" class="" alt="">
-				<p>Home system installation</p>
-			</div>
-			<div class="small-6 medium-3 columns">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/homepage/call-centre.png" class="" alt="">
-				<p>24 hour call centre</p>
-			</div>
-			<div class="small-6 medium-3 columns">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/homepage/gp-monitoring.png" class="" alt="">
-				<p>GP-led monitoring team</p>
+			<div class="row">
+				<div class="small-6 medium-3 columns">
+					<div class="dealIcon">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/portable-alarm.svg" class="" alt="">
+					</div>
+					<p>Portable <br>Alarm</p>
+				</div>
+				<div class="small-6 medium-3 columns">
+					<div class="dealIcon">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/home-installation.svg" class="" alt="">
+					</div>
+					<p>Home system <br>installation</p>
+				</div>
+				<div class="small-6 medium-3 columns">
+					<div class="dealIcon">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/call-centre.svg" class="" alt="">
+					</div>
+					<p>24 hour <br>call centre</p>
+				</div>
+				<div class="small-6 medium-3 columns">
+					<div class="dealIcon">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/icons/gp-monitoring.svg" class="" alt="">
+					</div>
+					<p>GP-led <br>monitoring team</p>
+				</div>
 			</div>
 		</div>
 	</section>
